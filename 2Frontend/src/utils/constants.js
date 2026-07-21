fb12d970c2117cefcee08a6eff588c303b9c74ff
SanskuAi <sanskarlb13@gmail.com>
2026-07-21 23:58:26 +0545

    add js

---
+++ 2Frontend/src/utils/constants.js
@@ -0,0 +1,15 @@
+function StudentDashboard() {
+    return (
+      <div>
+        <h1>Student Dashboard</h1>
+  
+        <h2>Welcome, Raj Kumar</h2>
+  
+        <p>Attendance: 85%</p>
+        <p>Marks: 78%</p>
+        <p>Fees Status: Paid</p>
+      </div>
+    );
+  }
+  
+  export default StudentDashboard;
